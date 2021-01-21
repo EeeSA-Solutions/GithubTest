104bc85d378ebe966a9c4cc055fa35bbfe7876c5
Wadstrom <73141419+Wadstrom@users.noreply.github.com>
2021-01-21 10:10:05 +0100

    metod och cw

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		string val;
+        Console.WriteLine("funkar detta?");
+		public void ja()
+		{ val = Console.ReadLine()
+
+                Console.WriteLine(val);
+        }
+	}
+}
